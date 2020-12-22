964d518e2bd5ddee65a30aa3859259a99c8b7ff6
Albert Volkman <albert.volkman@gmail.com>
2020-12-22 10:32:08 -0500

    Fix player reference to play videos

--- js/rebellion.js
+++ js/rebellion.js
@@ -80,13 +80,16 @@ function onYouTubeIframeAPIReady() {
   });
 
   function onPlayerReady(event) {
-    $('#' + event.target.f.id).parent().find('a').on('click', function() {
-      var overlay = $(this);
-      var video = overlay.parent().find('.video');
+    Object.keys(players).map(function(id) {
+      $('#' + id).parent().find('a').on('click', function() {
+        var overlay = $(this);
+        var video = overlay.parent().find('.video');
 
-      overlay.hide();
-      video.show();
-      event.target.playVideo();
+        // Hide overlay, show and start video.
+        overlay.hide();
+        video.show();
+        players[id].playVideo();
+      });
     });
   }
 }
